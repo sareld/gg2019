cbda1ed331f9f42f3c752d995a18e2381a5afb47
ZBOX\asaf <gevasaf@gmail.com>
2019-01-25 13:52:23 +0200

    dirt score

--- Assets/cleaning system/DirtScore.cs
+++ Assets/cleaning system/DirtScore.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class DirtScore : MonoBehaviour
 {
-    public RenderTexture DirtTex;
-    float score;
-    
-
-    // Update is called once per frame
-    void Update()
+    public RenderTexture dirtTex;
+    public Texture2D tex;
+    public float score;
+    public float maxScore = 65536;
+    private void Awake()
     {
-        
+        tex = new Texture2D(dirtTex.width,dirtTex.height, TextureFormat.RGB24, false);
+    }
+    private void Update()
+    {
+        score = 0;
+        RenderTexture.active = dirtTex;
+        tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+        for(int i = 0; i < tex.width; i++){
+            for(int j = 0; j < tex.height; j++){
+                score+=tex.GetPixel(i,j).r;
+            }
+        }
+        score = score/maxScore;
+    }
+    public GUIStyle style;
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20),"dirt % "+ Mathf.RoundToInt(score*100).ToString(), style);
     }
 }
