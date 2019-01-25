1747d486163dab265d7fbffd50aa913beb640823
ZBOX\asaf <gevasaf@gmail.com>
2019-01-25 12:34:05 +0200

    display dirt on floor

---
+++ Assets/cleaning system/DirtScore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtScore : MonoBehaviour
+{
+    public RenderTexture DirtTex;
+    float score;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
